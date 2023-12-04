13ec626bd15a7f8b9e566077b5f9fb17801cb04f
greeneeya <suzythetravelerr@gmail.com>
2023-12-04 13:31:07 +0900

    test: Validation test 추가

--- src/Validation.js
+++ src/Validation.js
@@ -1,4 +1,4 @@
-import { LOTTO_RULE } from './constants/constants';
+import { ERROR_MESSAGE, LOTTO_RULE } from './constants/constants';
 
 export default class Validation {
   static validateLotto(numbers) {
@@ -19,20 +19,20 @@ export default class Validation {
   }
 
   static isSixNumbers(numbers) {
-    if (numbers.length !== LOTTO_RULE.pickableNumbers) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    if (numbers.length !== LOTTO_RULE.mustPickNumbers) {
+      throw new Error(ERROR_MESSAGE.isSixNumbers);
     }
   }
 
   static isUniqueNumbers(numbers) {
-    if (new Set(numbers).size !== LOTTO_RULE.pickableNumbers) {
-      throw new Error('[ERROR] 각 로또 번호는 중복될 수 없습니다.');
+    if (new Set(numbers).size !== LOTTO_RULE.mustPickNumbers) {
+      throw new Error(ERROR_MESSAGE.mustUniqueNumbers);
     }
   }
 
   static isUniqueNumber(winningNumbers, number) {
     if (winningNumbers.includes(number)) {
-      throw new Error('[ERROR] 로또 번호와 중복된 수는 입력 불가합니다.');
+      throw new Error(ERROR_MESSAGE.mustUniqueNumber);
     }
   }
 
@@ -45,19 +45,19 @@ export default class Validation {
 
   static isInteger(number) {
     if (!Number.isInteger(number)) {
-      throw new Error('[ERROR] 로또 번호는 1-45 사이의 정수만 가능합니다.');
+      throw new Error(ERROR_MESSAGE.isWithinRange);
     }
   }
 
   static isInRange(number) {
     if (number < LOTTO_RULE.minNum || number > LOTTO_RULE.maxNum) {
-      throw new Error('[ERROR] 로또 번호는 1-45 사이의 정수만 가능합니다.');
+      throw new Error(ERROR_MESSAGE.isWithinRange);
     }
   }
 
   static isDivisible(paid) {
     if (paid < LOTTO_RULE.ticketPrice || paid % LOTTO_RULE.ticketPrice !== 0) {
-      throw new Error('[ERROR] 1000원 단위로 구매 가능합니다.');
+      throw new Error(ERROR_MESSAGE.isDivisible);
     }
   }
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ const LOTTO_RULE = Object.freeze({
   ticketPrice: 1000,
   maxNum: 45,
   minNum: 1,
-  pickableNumbers: 6,
+  mustPickNumbers: 6,
 });
 const PRIZE = Object.freeze({
   three: 5000,
@@ -11,5 +11,12 @@ const PRIZE = Object.freeze({
   bonusFive: 30000000,
   six: 2000000000,
 });
+const ERROR_MESSAGE = Object.freeze({
+  isSixNumbers: '[ERROR] 로또 번호는 6개여야 합니다.',
+  mustUniqueNumbers: '[ERROR] 각 로또 번호는 중복될 수 없습니다.',
+  mustUniqueNumber: '[ERROR] 로또 번호와 중복된 수는 입력 불가합니다.',
+  isWithinRange: '[ERROR] 로또 번호는 1-45 사이의 정수만 가능합니다.',
+  isDivisible: '[ERROR] 1000원 단위로 구매 가능합니다.',
+});
 
-export { LOTTO_RULE, PRIZE };
+export { LOTTO_RULE, PRIZE, ERROR_MESSAGE };
